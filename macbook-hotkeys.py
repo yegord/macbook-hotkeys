968e5a208e14db109b00bd64e51443c1d9a58b56
Yegor Derevenets <yegor.derevenets@gmail.com>
2015-01-25 04:22:12 +0100

    Not trying to set negative brightness

--- macbook-hotkeys.py
+++ macbook-hotkeys.py
@@ -5,7 +5,42 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from evdev import InputDevice, KeyEvent, ecodes
-from os import system
+import os
+
+class Backlight(object):
+    def __init__(self, dirname):
+        self.dirname = dirname
+        self.brightness_filename = os.path.join(self.dirname, 'brightness')
+        self.max_brightness_filename = os.path.join(self.dirname, 'max_brightness')
+
+    def get_min_brightness(self):
+        return 0
+
+    def get_max_brightness(self):
+        with open(self.max_brightness_filename, 'r') as f:
+            return int(f.read())
+
+    def get_brightness(self):
+        with open(self.brightness_filename, 'r') as f:
+            return int(f.read())
+
+    def set_brightness(self, value):
+        value = max(value, self.get_min_brightness())
+        value = min(value, self.get_max_brightness())
+        with open(self.brightness_filename, 'w') as f:
+            f.write(str(value))
+
+    def increase_brightness(self, delta):
+        self.set_brightness(self.get_brightness() + delta)
+
+    def decrease_brightness(self, delta):
+        self.increase_brightness(-delta)
+
+screen = Backlight('/sys/class/backlight/intel_backlight')
+keyboard = Backlight('/sys/class/leds/smc::kbd_backlight')
+
+screen_delta = 10
+keyboard_delta = 10
 
 dev = InputDevice('/dev/input/event0')
 
@@ -13,10 +48,10 @@ for event in dev.read_loop():
     if event.type == ecodes.EV_KEY:
         if event.value in [KeyEvent.key_up, KeyEvent.key_hold]:
             if event.code == ecodes.KEY_BRIGHTNESSDOWN:
-                system('expr `cat /sys/class/backlight/intel_backlight/brightness` - 10 > /sys/class/backlight/intel_backlight/brightness')
+                screen.decrease_brightness(screen_delta)
             if event.code == ecodes.KEY_BRIGHTNESSUP:
-                system('expr `cat /sys/class/backlight/intel_backlight/brightness` + 10 > /sys/class/backlight/intel_backlight/brightness')
+                screen.increase_brightness(screen_delta)
             if event.code == ecodes.KEY_KBDILLUMDOWN:
-                system('expr `cat /sys/class/leds/smc::kbd_backlight/brightness` - 10 > /sys/class/leds/smc::kbd_backlight/brightness')
+                keyboard.decrease_brightness(keyboard_delta)
             if event.code == ecodes.KEY_KBDILLUMUP:
-                system('expr `cat /sys/class/leds/smc::kbd_backlight/brightness` + 10 > /sys/class/leds/smc::kbd_backlight/brightness')
+                keyboard.increase_brightness(keyboard_delta)
